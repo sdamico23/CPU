971f3bcac8b43786dd9bc1d48509287da9b08300
sdamico23 <sdamico1023@gmail.com>
2019-12-05 13:33:24 -0500

    Add files via upload

--- Lab3Source.v
+++ Lab3Source.v
@@ -48,43 +48,55 @@ endmodule
 
 module IM(
     input [31:0] pc,
-    output reg[31:0]instruction
+    output reg[31:0]do
     );
-reg [31:0]instructionmemory[511:0];
+reg [31:0]instructionmemory[0:511];
 
 
     initial
     begin 
     instructionmemory[100] = 32'h8c220000;
-    instructionmemory[104] = 32'h8c230004;      
+    instructionmemory[104] = 32'h8c230004; 
+    instructionmemory[108] = 32'h8c240008;
+    instructionmemory[112] = 32'h8c25000c;  
+    instructionmemory[116]= 32'h004a3020;   
     end 
      
      always @(pc)
      begin
-     instruction <= instructionmemory[pc];
+     do <= instructionmemory[pc];
      end
   
 endmodule
 
 
 
-module IFID( clk, instruction, opcode, rt, rs, rd, imm
-
+module IFID( clk, do, opcode, rt, rs, rd, imm, shift,funcode
 );
 input clk;
-input [31:0]instruction;
-output reg [5:0]opcode;
-output reg[4:0]rs, rt, rd;
+input [31:0]do;
+output reg [5:0]opcode, funcode;
+output reg[4:0]rs, rt, rd, shift;
 output reg [15:0]imm;
 
     always @(posedge clk)
     begin
-    opcode<=instruction[31:26];
-    rs <= instruction[25:21];
-    rt <= instruction[20:16];
-    rd <= instruction[15:11];
-    imm <= instruction[15:0];
+    if (do[31:26]== 6'b100011) begin
+    opcode<=do[31:26];
+    rs <= do[25:21];
+    rt <= do[20:16];
+    imm <= do[15:0];
     end
+    
+    else begin
+    opcode<=do[31:26];
+    rs <= do[25:21];
+    rt <= do[20:16];
+    rd <= do[15:11]; 
+    funcode <= do[5:0];
+    end
+    end
+    
 endmodule 
 
 
@@ -104,15 +116,23 @@ output reg regrt;
     6'b100011:
     begin 
         aluc <=4'b0010;
-        wreg=1'b1; 
-        m2reg = 1'b1; 
-        wmem=1'b0; 
-        aluimm=1'b1; 
-        regrt=1'b1;
+        wreg<=1'b1; 
+        m2reg <= 1'b1; 
+        wmem<=1'b1; 
+        aluimm<=1'b1; 
+        regrt<=1'b1;
+    end
+    6'b000000:
+    begin
+        aluc <=4'b0010;
+        wreg<=1'b1; 
+        m2reg <= 1'b0; 
+        wmem<=1'b0; 
+        aluimm<=1'b0; 
+        regrt<=1'b0; 
     end
     endcase
     end
-
 endmodule
     
 
@@ -137,11 +157,15 @@ endmodule
 
 
 module regMem(
+input clk,
+input wwreg,
+input [4:0]wmux,
+input [31:0]d,
 input [4:0]rs,
 input [4:0]rt,
 output reg[31:0]qa,
 output reg[31:0]qb
-    );
+);
 reg [31:0]regfile[31:0];
 
     //always @(negedge clk)
@@ -150,10 +174,14 @@ integer i;
     for (i=0;i<32; i= i+1)
         regfile[i] = 0;
         end
-     always @(*) begin
-        qa <= regfile[rs];
-        qb <= regfile[rt];
-     end
+     always @(negedge clk) begin
+      if (wwreg)
+      regfile[wmux] = d;
+      end
+      always @(*)begin
+       qa <= regfile[rs];
+       qb <= regfile[rt];
+       end
 
 endmodule
 
@@ -181,8 +209,8 @@ input wmem;
 input [3:0]aluc;
 input aluimm;
 input [4:0]mux;
-input [4:0]qa;
-input [4:0]qb;
+input [31:0]qa;
+input [31:0]qb;
 input [31:0]signextendimm; 
 output reg ewreg;
 output reg em2reg;
@@ -190,8 +218,8 @@ output reg ewmem;
 output reg [3:0]ealuc;
 output reg ealuimm;
 output reg[4:0]emux;
-output reg[4:0]eqa;
-output reg[4:0]eqb;
+output reg[31:0]eqa;
+output reg[31:0]eqb;
 output reg[31:0]esignextendimm; 
 
     always @(posedge clk)
@@ -208,3 +236,122 @@ output reg[31:0]esignextendimm;
     end
     
 endmodule 
+
+module mux2(ealuimm, eqb, esignextendimm, b);
+
+input ealuimm;
+input [31:0]eqb;
+input [31:0]esignextendimm;
+output reg [31:0]b;
+
+    always @(*)
+    if (ealuimm == 1'b1) begin
+    b <= esignextendimm;
+    end
+    else begin
+    b <= eqb;
+    end
+endmodule
+
+module ALU(ealuc, a, b, r);
+input [31:0]a;
+input [31:0] b;
+input [3:0]ealuc;
+output reg [31:0]r;
+
+always @(*) begin
+if (ealuc == 4'b0010) begin
+    r <= a+b;
+    end
+  end
+endmodule
+
+
+module exemem(clk, ewreg, em2reg, ewmem, emux, r, eqb, mwreg, mm2reg, mwmem, mmux, a, di);
+input ewreg, em2reg, ewmem;
+input clk;
+input [4:0]emux;
+input [31:0]r;
+input [31:0]eqb;
+output reg mwreg, mm2reg, mwmem;
+output reg [4:0]mmux;
+output reg[31:0]a;
+output reg[31:0]di;
+
+    always @(posedge clk)
+    begin
+    mwreg <= ewreg;
+    mm2reg <=em2reg;
+    mwmem <= ewmem;
+    mmux <= emux;
+    a <= r;
+    di <= eqb;
+    end
+    
+endmodule
+
+module datamem(a, di, we, do2);
+
+input [31:0]a;
+input [31:0]di;
+input we;
+output reg [31:0]do2;
+reg [31:0]datamemory[511:0];
+
+
+    initial
+    begin 
+    datamemory[0] = 32'hA00000AA;
+    datamemory[4] = 32'h10000011; 
+    datamemory[8] = 32'h20000022;
+    datamemory[12] = 32'h30000033;
+    datamemory[16] = 32'h40000044;
+    datamemory[20] = 32'h50000055;
+    datamemory[24] = 32'h60000066;
+    datamemory[28] = 32'h70000077;
+    datamemory[32] = 32'h80000088;
+    datamemory[36] = 32'h90000099;
+         
+    end 
+  
+     always @(*)
+     if (we==1) begin
+     do2 <= datamemory[a];
+     end
+  
+endmodule
+
+module memwb(clk, mwreg, mm2reg, a, mmux, do2, wwreg, wm2reg, wa, wmux, wdo);
+input clk;
+input mwreg, mm2reg; 
+input [31:0]a, do2;
+input [4:0]mmux;
+output reg wwreg, wm2reg;
+output reg [31:0]wa, wdo;
+output reg [4:0]wmux;
+
+always @(posedge clk) begin
+    wwreg <= mwreg;
+    wm2reg <=mm2reg;
+    wa <=a;
+    wdo <= do2;
+    wmux <=mmux;
+    end
+endmodule
+
+module mux3(wm2reg, wdo, wa, d);
+input wm2reg;
+input [31:0] wa,wdo;
+output reg[31:0] d;
+
+    always @(*)
+    if (wm2reg == 1'b1) begin
+    d <= wdo;
+    end
+    else begin
+    d <= wa;
+    end
+endmodule
+    
+
+
--- testBench.v
+++ testBench.v
@@ -25,29 +25,29 @@ module testBench(    );
 reg clk;
 wire [31:0]currentpc;
 wire [31:0]nextpc;
-wire [31:0]instruction;
+wire [31:0]do, do2;
 wire [31:0]qa;
 wire [31:0]qb; 
 wire [31:0]signextendimm;
-wire [5:0]op;
+wire [5:0]op, funcode;
 wire [4:0]rs;
-wire [4:0] rt;
+wire [4:0]rt;
 wire [4:0]rd;
 wire [4:0]out;
-wire [4:0]emux;
-wire [4:0]eqa;
-wire [4:0]eqb;
-wire [4:0]esignextendimm;
+wire [4:0]emux, mmux, wmux;
+wire [31:0]eqa;
+wire [31:0]eqb;
+wire [31:0]esignextendimm, b,r, a, di, wa, wdo,d;
 wire [15:0]imm;
-wire wreg;
-wire m2reg;
-wire wmem;
-wire aluimm;
+wire wreg, ewreg;
+wire m2reg, em2reg, mwreg, mm2reg, mwmem, wwreg, wm2reg;
+wire wmem, ewmem;
+wire aluimm, ealuimm;
 wire regrt;
-wire [3:0] aluc;
+wire [3:0] aluc, ealuc;
 
 initial begin
-clock = 0;
+clk = 0;
 end
 always 
 #5 clk = !clk;
@@ -55,14 +55,18 @@ always
 
 PC pc_tb(.clk(clk), .nextpc(nextpc), .currentpc(currentpc));
 adder adder_tb(.pc(currentpc), .nextpc(nextpc));
-IM im_tb(.pc(currentpc), .instruction(instruction));
-IFID ifid_tb(.clk(clk), .instruction(instruction), .opcode(op), .rt(rt), .rs(rs), .rd(rd), .imm(imm));
+IM im_tb(.pc(currentpc), .do(do));
+IFID ifid_tb(.clk(clk), .do(do), .opcode(op), .rt(rt), .rs(rs), .rd(rd),.funcode(funcode), .imm(imm));
 controlUnit cu_tb(.opcode(op), .wreg(wreg), .m2reg(m2reg), .wmem(wmem), .aluc(aluc), .aluimm(aluimm), .regrt(regrt));
 mp mp_tb(.rd(rd), .rt(rt), .regrt(regrt), .out(out));
-regMem rm_tb(.rs(rs), .rt(rt), .qa(qa), .qb(qb));
+regMem rm_tb(.rs(rs), .rt(rt),.clk(clk), .wwreg(wwreg), .wmux(wmux), .d(d), .qa(qa), .qb(qb));
 signExtend se_tb(.imm(imm), .signextendimm(signextendimm));
 IDEXE idexe_tb(.clk(clk), .wreg(wreg), .m2reg(m2reg), .wmem(wmem), .aluc(aluc), .aluimm(aluimm), .mux(out), .qa(qa), .qb(qb), .signextendimm(signextendimm), .ewreg(ewreg), .em2reg(em2reg), .ewmem(ewmem), .ealuc(ealuc), .ealuimm(ealuimm), .emux(emux), .eqa(eqa), .eqb(eqb), .esignextendimm(esignextendimm));
-
-
+mux2 mux2_tb(.ealuimm(ealuimm), .eqb(eqb), .esignextendimm(esignextendimm), .b(b));
+ALU alu_tb(.ealuc(ealuc), .a(eqa), .b(b), .r(r));
+exemem exemem_tb(.clk(clk), .ewreg(ewreg), .em2reg(em2reg), .ewmem(ewmem), .emux(emux), .r(r), .eqb(eqb), .mwreg(mwreg), .mm2reg(mm2reg), .mwmem(mwmem), .mmux(mmux), .a(a), .di(di));
+datamem datamem_tb(.a(a), .di(di), .we(mwmem), .do2(do2));
+memwb memwb_tb(.clk(clk), .mwreg(mwreg), .mm2reg(mm2reg), .a(a), .mmux(mmux), .do2(do2), .wm2reg(wm2reg), .wa(wa), .wmux(wmux),.wwreg(wwreg), .wdo(wdo));
+mux3 mux3_tb(.wm2reg(wm2reg), .wdo(wdo), .wa(wa), .d(d));
 
 endmodule
